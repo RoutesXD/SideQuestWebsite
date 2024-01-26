3a77fc73fff04d95eb1234459870fd0271bb946d
Samarvir Singh Arora <89194124+RoutesXD@users.noreply.github.com>
2024-01-26 15:45:36 +0530

    main build + navbar
    used vite for main build and also imported tailwind

---
+++ demo/demo/src/components/index.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
